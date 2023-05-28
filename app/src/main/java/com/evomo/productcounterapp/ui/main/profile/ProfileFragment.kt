613c825efa60c05249006a262c159c9d2f64cd30
David Johan Hasiholan Parhusip <david.johanhp@gmail.com>
2023-05-29 00:02:51 +0700

    add logout warning modal

--- app/src/main/java/com/evomo/productcounterapp/ui/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/evomo/productcounterapp/ui/main/profile/ProfileFragment.kt
@@ -1,24 +1,27 @@
 package com.evomo.productcounterapp.ui.main.profile
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
+import android.view.Window
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.evomo.productcounterapp.R
-import com.evomo.productcounterapp.databinding.FragmentHomeBinding
 import com.evomo.productcounterapp.databinding.FragmentProfileBinding
-import com.evomo.productcounterapp.ui.camera.CameraActivity
 import com.evomo.productcounterapp.ui.login.LoginActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
@@ -51,7 +54,44 @@ class ProfileFragment : Fragment() {
 //            binding.userMobile.text = firebaseUser.phoneNumber.toString()
 
             binding.logoutGroup.setOnClickListener {
-                signOut()
+                val builder = AlertDialog.Builder(activity, R.style.LogoutDialog)
+                with(builder) {
+                    setTitle(R.string.modal_logout_title)
+                    setMessage(R.string.modal_logout_desc)
+                    setNegativeButton(R.string.btn_cancel) { dialogInterface: DialogInterface, i: Int ->
+                        dialogInterface.cancel()
+                    }
+                    setPositiveButton(R.string.btn_logout) { dialogInterface: DialogInterface, i: Int ->
+                        signOut()
+                    }
+                    setIcon(R.drawable.ic_baseline_warning_24_yellow);
+                }
+
+                val alertDialog = builder.create()
+                alertDialog.show()
+
+                val positiveButton = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE)
+                with(positiveButton) {
+                    isAllCaps = false
+                    setTextColor(resources.getColor(R.color.red))
+                }
+
+                val negativeButton = alertDialog.getButton(DialogInterface.BUTTON_NEGATIVE)
+                with(negativeButton) {
+                    isAllCaps = false
+                    setTextColor(resources.getColor(R.color.black))
+                }
+
+//                val title = alertDialog.findViewById<TextView>(android.R.id.title)
+//                title.typeface = activity?.let { it1 -> ResourcesCompat.getFont(it1, R.font.poppins_semibold) }
+//                AlertDialog.Builder(activity)
+//                    .setTitle(R.string.modal_logout_title)
+//                    .setMessage(R.string.modal_logout_desc)
+//                    .setNegativeButton(R.string.btn_cancel, null)
+//                    .setPositiveButton(R.string.btn_logout) { dialogInterface: DialogInterface, i: Int ->
+//                        signOut()
+//                    }.create().show()
+//                signOut()
             }
         }
     }
@@ -67,5 +107,6 @@ class ProfileFragment : Fragment() {
     }
 
     companion object {
+
     }
 }
